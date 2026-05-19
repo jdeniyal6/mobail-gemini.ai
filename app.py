c2918b2a0932cc709fac6dbd2699e534bfc573b3
jdeniyal6 <jdeniyal6@gmail.com>
2026-05-19 16:58:01 +0530

    Refactor Flask app to handle JSON requests

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, send_file
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app)
+
 @app.route("/")
 def home():
-    return send_file("index.html")
+    return "Gemini Backend Running"
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    return {
-        "reply": "Hello 👋 Gemini Mobile AI Backend Working!"
-    }
+
+    data = request.get_json()
+
+    user_message = data.get("message", "")
+
+    return jsonify({
+        "reply": "Hello 👋 You said: " + user_message
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
